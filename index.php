0f6f633f9dac5b4569959d60d70544005d81d82f
yemdom ngueussie dominique <ngueussievega@gmail.com>
2022-06-24 11:37:28 +0100

    modif du fichier image index.php

--- index.php
+++ index.php
@@ -8,9 +8,14 @@
     </script>
 </head>
 
-<body style="background-image:url('./images/slider-bg.png');background-attachment:fixed;background-position:center;background-size:cover;background-repeat:no-repeat;text-align:center;margin-top:5%;" onLoad="setTimeout('RedirectionJavascript()', 2000)">
+<!--<body style="background-image:url('./images/slider-bg.png');background-attachment:fixed;background-position:center;background-size:cover;background-repeat:no-repeat;text-align:center;margin-top:5%;" onLoad="setTimeout('RedirectionJavascript()', 2000)">
     <div style="margin:auto;">
-       <img style="opacity:.8" src="./images/loaders/heart-loading2.gif" alt="">
+       <img class="img-thumbnail" alt="Cinque Terre" width="400" height="400" style="opacity:.8" src="./images/loaders/heart-loading2.gif" alt="">
+    </div>
+</body>-->
+<body style="background-color:#60c2ae; text-align:center;margin-top:5%;" onLoad="setTimeout('RedirectionJavascript()', 2000)">
+    <div style="margin:auto;">
+       <img class="img-thumbnail" alt="Cinque Terre" width="400" height="400" style="opacity:.8" src="./images/loaders/heart-loading2.gif" alt="">
     </div>
 </body>
 
