59550e830f7057766cce4ed212a9af01571a5669
yemdom ngueussie dominique <ngueussievega@gmail.com>
2022-06-24 04:42:09 +0100

    finalisation de la nav-bar partie1

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <script type="text/javascript">
+        function RedirectionJavascript() {
+            document.location.href = "http://localhost/projectHealth/pages/accueil.html";
+        }
+    </script>
+</head>
+
+<body style="background-image:url('./images/slider-bg.png');background-attachment:fixed;background-position:center;background-size:cover;background-repeat:no-repeat;" onLoad="setTimeout('RedirectionJavascript()', 2000)">
+    <div style="position:relative;top:5rem;left:25rem;">
+       <img style="opacity:.8" src="./images/loaders/heart-loading2.gif" alt="">
+    </div>
+</body>
+
+</html>
