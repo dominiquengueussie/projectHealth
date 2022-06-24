5704dd95b9151c65741c884408ef25619f69158a
yemdom ngueussie dominique <ngueussievega@gmail.com>
2022-06-24 11:16:39 +0100

    modif de la nav partie 3

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     </script>
 </head>
 
-<body style="background-image:url('./images/slider-bg.png');background-attachment:fixed;background-position:center;background-size:cover;background-repeat:no-repeat;" onLoad="setTimeout('RedirectionJavascript()', 2000)">
-    <div style="position:relative;top:5rem;left:25rem;">
+<body style="background-image:url('./images/slider-bg.png');background-attachment:fixed;background-position:center;background-size:cover;background-repeat:no-repeat;text-align:center;margin-top:5%;" onLoad="setTimeout('RedirectionJavascript()', 2000)">
+    <div style="margin:auto;">
        <img style="opacity:.8" src="./images/loaders/heart-loading2.gif" alt="">
     </div>
 </body>
